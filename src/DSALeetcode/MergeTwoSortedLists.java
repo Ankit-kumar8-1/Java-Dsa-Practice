0acbc580f2ad32a43c4e2533d2d3874aacf0bb18
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2025-11-06 20:40:45 +0530

    Solved ( Merge Two Sorted Lists Using Recursion   ) Problem .

--- src/DSALeetcode/MergeTwoSortedLists.java
+++ src/DSALeetcode/MergeTwoSortedLists.java
@@ -32,4 +32,21 @@ public class MergeTwoSortedLists {
 
         return  dummy.next;
     }
+
+
+//    Using Recursion
+
+    public  static  ListNode mergeTwoLists2(ListNode list1 , ListNode list2){
+        if(list1 == null) return  list2;
+        if(list2 ==null ) return  list1;
+
+        if(list1.val < list2.val){
+            list1.next = mergeTwoLists2(list1.next , list2);
+            return  list1;
+
+        }else {
+            list2.next = mergeTwoLists2(list1,list2.next);
+            return  list2;
+        }
+    }
 }
