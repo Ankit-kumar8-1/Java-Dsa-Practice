2de8fa0593498ffb5a03b24eb283fd81f73fadf7
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-01-30 21:24:02 +0530

    solved Evaluate Reverse Polish Notation

---
+++ src/DSALeetcode/quest/EvaluateReversePolishNotation.java
@@ -0,0 +1,37 @@
+package DSALeetcode.quest;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+
+        for(String token : tokens){
+            if(token.equals("+") || token.equals("-")
+            || token.equals("*") || token.equals("/")){
+
+                int b = stack.pop();
+                int a =  stack.pop();
+
+                int result = 0;
+                if(token.equals("+")) result = a+b;
+                else if(token.equals("-")) result = a-b;
+                else if(token.equals("*")) result = a*b;
+                else result = a/b;
+
+                stack.push(result);
+            }else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String tokens [] = {"2","1","+","3","*"};
+        System.out.println(evalRPN(tokens));
+    }
+}
