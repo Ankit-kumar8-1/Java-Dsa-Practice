d434eaf152d890049dade48bfaf7a99afb9f7445
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-05-07 06:06:01 +0530

    optimize soltuion

--- src/Graph/DFS.java
+++ src/Graph/DFS.java
@@ -26,6 +26,17 @@ public class DFS {
 
     }
 
+    public  static  void dfs2(ArrayList<Edge> graph[],int curr, boolean visited[]){
+        System.out.println(curr+" ");
+        visited[curr]= true;
+        for (int i = 0 ;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            if (visited[e.dest]==false){
+                dfs2(graph,e.dest,visited);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         CreateGraph createGraph = new CreateGraph();
         int vertix = 7;
@@ -34,9 +45,16 @@ public class DFS {
         int start = 0;
         createGraph.createGraph(graph);
 
-        dfs(graph,0,visited);
+//        dfs(graph,0,visited);
+        System.out.println("more Optimize  solution");
+//        dfs2(graph,0,visited);
 
 
+        for (int i = 0;i<vertix;i++){
+            if (visited[i]==false){
+                dfs2(graph,0,visited);;
+            }
+        }
 
 
     }
